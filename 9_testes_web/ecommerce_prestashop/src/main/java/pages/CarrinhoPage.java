927ca95c1640cf675a4d31381be5f169df83312c
Marcelo de Bittencourt <tchelopoa@gmail.com>
2020-06-05 04:54:53 -0300

    Mapeamento dos elementos do carrinho

--- 9_testes_web/ecommerce_prestashop/src/main/java/pages/CarrinhoPage.java
+++ 9_testes_web/ecommerce_prestashop/src/main/java/pages/CarrinhoPage.java
@@ -11,6 +11,27 @@ public class CarrinhoPage {
 	
 	private By precoProduto = By.cssSelector("span.price");	
 	
+	private By tamanhoProduto = By.xpath("//div[contains(@class,'product-line-grid-body')]//div[3]/span[contains(@class,'value')]");
+	
+	private By corProduto = By.xpath("//div[contains(@class,'product-line-grid-body')]//div[4]/span[contains(@class,'value')]");
+	
+	private By input_quantidadeProduto = By.cssSelector("input.js-cart-line-product-quantity");
+	
+	private By subtotalProduto = By.cssSelector("span.product-price strong");	
+	
+	private By numeroItensTotal = By.cssSelector("span.js-subtotal");	
+	
+	private By subtotalTotal = By.cssSelector("#cart-subtotal-products span.value");	
+	
+	private By shippingTotal = By.cssSelector("#cart-subtotal-shipping span.value");	
+	
+	private By totalTaxExclTotal = By.cssSelector("div.cart-summary-totals div.cart-summary-line:nth-child(1) span.value");	
+	
+	private By totalTaxIncTotal = By.cssSelector("div.cart-summary-totals div.cart-summary-line:nth-child(2) span.value");	
+	
+	private By taxesTotal = By.cssSelector("div.cart-summary-totals div.cart-summary-line:nth-child(3) span.value");	
+   
+	
 	//private By tamanhoProduto = By ("");	
 	
 	public CarrinhoPage(WebDriver driver) {
